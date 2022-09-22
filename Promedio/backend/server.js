cfac9f3838be2055fccd705a0385f8226f8977ee
kingpreyansh <preyanshdutta@gmail.com>
2022-09-22 11:50:09 -0400

    Added server.js

--- Promedio/backend/server.js
+++ Promedio/backend/server.js
@@ -1,19 +1,18 @@
-//console.log("Hello, Promedio")
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config({ path: "./config.env" });
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+// app.use(require("./routes/record"));
+// get driver connection
+const dbo = require("./db/connection");
 
-const express = require('express')
-const dotenv = require('dotenv').config() // .env file
-const server_port = process.env.SERVER_PORT || 5000
-const { connectDatabase } = require('./config/database')
-
-//connectDatabase()    Don't do it here, do it somewhere in frontend to showcase functionality
-
-const app = express()
-
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
-
-app.use('/api/sheets', require('./routes/sheetRoutes'))
-
-app.listen(server_port, () => {
-    console.log(`Server started on port ${server_port}`)
-})
+app.listen(port, () => {
+  // perform a database connection when server starts
+  dbo.connectToServer(function (err) {
+    if (err) console.error(err);
+  });
+  console.log(`Server is running on port: ${port}`);
+});
