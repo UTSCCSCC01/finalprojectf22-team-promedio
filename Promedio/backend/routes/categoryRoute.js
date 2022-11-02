0e44d651d54a559bf08eec1cbfcb34cd7d0b7be6
kingpreyansh <preyanshdutta@gmail.com>
2022-11-01 23:30:44 -0400

    Fixed route

--- Promedio/backend/routes/categoryRoute.js
+++ Promedio/backend/routes/categoryRoute.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const CategoryController = require("../controllers/categoryController");
-router.post("/registerCategory", CategoryController.register);
-router.post("/editCategory", CategoryController.edit);
-router.post("/deleteCategory", CategoryController.delete);
+router.post("/registerCategory", CategoryController.addCategory);
+router.post("/editCategory", CategoryController.editCategory);
+router.post("/deleteCategory", CategoryController.deleteCategory);
 module.exports = router;
