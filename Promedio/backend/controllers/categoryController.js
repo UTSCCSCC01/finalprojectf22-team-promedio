df6ece3eb5cf8da3272572a2a795277ed7d69504
kingpreyansh <preyanshdutta@gmail.com>
2022-11-01 23:29:10 -0400

    Add category

--- Promedio/backend/controllers/categoryController.js
+++ Promedio/backend/controllers/categoryController.js
@@ -2,12 +2,6 @@ const category = require("../models/academicCategoryModel");
 
 // backend api controller to update a user's data based on their email
 const addCategory = (req, res, next) => {
-  const userId = req.body.id;
-  const courseId = req.body.name;
-  const categories = req.body.email;
-  const weight = req.body.email;
-  const mark = req.body.email;
-
   let newCategory = new category({
     userId: req.body.userId,
     courseId: req.body.courseId,
@@ -15,6 +9,28 @@ const addCategory = (req, res, next) => {
     weight: req.body.weight,
     mark: req.body.mark,
   });
+
+  newCategory
+    .save()
+    .then((newCategory) => {
+      if (newCategory) {
+        res.json({
+          message: "Category added successfully.",
+          category: {
+            ...newUser.toObject(),
+          },
+        });
+      } else {
+        next({
+          message: "Category not added.",
+          status: 500,
+          stack: "Category not added.",
+        });
+      }
+    })
+    .catch((err) => {
+      next(err);
+    });
 };
 
 module.exports = {
